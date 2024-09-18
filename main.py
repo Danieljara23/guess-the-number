770511f2faa539618bf13f386ca45c7bec132f6c
Isabella R <urramirou0507@gmail.com>
2024-09-18 11:18:40 -0500

    Codigo Arreglado

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ def adivina_competencia():
     
     print("¡Bienvenido al juego Guess The Number!")
     print("He pensado un número entre 1 y 100. Tú y la máquina intentarán adivinarlo.")
+    print("----------------------------------------------")
     
-    intentos_usuario = 0
     intentos_maquina = 0
+    intentos_usuario = 0
     min_rango = 1
     max_rango = 100
     adivinado = False
@@ -16,39 +17,39 @@ def adivina_competencia():
     while not adivinado:
         # Turno del usuario
         intento_usuario = int(input("Tu turno, introduce tu intento: "))
-        intentos_usuario += 1
+        intento_usuario += 1
         
         if intento_usuario < numero_secreto:
-            print("El número es mayor.")
+            print("El número que intentas adivinar es mayor.")
+            print("-------------------------")
+
         elif intento_usuario > numero_secreto:
-            print("El número es menor.")
+            print("El número que intentas adivinar es menor.")
+            print("-------------------------")
+            
         else:
-            print(f"¡Felicidades! Adivinaste el número en {intentos_usuario} intentos.")
+            print(f"¡Felicidades! Adivinaste el número.")
             adivinado = True
             continue
         
         # Turno de la máquina
         intento_maquina = random.randint(min_rango, max_rango)
-        intentos_maquina += 1
+        intento_maquina += 1
+        print("-------------------------")
         print(f"La máquina intenta con el número: {intento_maquina}")
         
         if intento_maquina < numero_secreto:
-            print("La máquina falló, su intento es menor.")
+            print("La máquina falló, su número es menor.")
             min_rango = intento_maquina + 1
+            print("-------------------------")
         elif intento_maquina > numero_secreto:
-            print("La máquina falló, su intento es mayor.")
+            print("La máquina falló, su número es mayor.")
             max_rango = intento_maquina - 1
+            print("-------------------------")
         else:
-            print(f"¡La máquina ha adivinado el número en {intentos_maquina} intentos!")
+            print(f"¡La máquina ha adivinado el número primero que tu!")
             adivinado = True
-    
-    # Fin del juego
-    if intentos_usuario < intentos_maquina:
-        print(f"\n¡Ganaste! Lo lograste en {intentos_usuario} intentos contra {intentos_maquina} de la máquina.")
-    elif intentos_usuario > intentos_maquina:
-        print(f"¡La máquina ganó! \n Adivinó en {intentos_maquina} intentos, mientras que tú lo intentaste {intentos_usuario} veces.")
-    else:
-        print(f"\n¡Empate! Ambos adivinaron el número en {intentos_usuario} intentos.")
+            break
 
 # Ejecutar el juego
 adivina_competencia()
