08dd832fd080a74cbbe826511bc92d15ea87eb8d
Kalel Molano <contagiousmisfortune@gmail.com>
2024-09-20 06:21:44 -0500

    Todo el código hecho hasta el punto de llevar el contador de intentos antes de ganar

---
+++ main.py
@@ -0,0 +1,33 @@
+print("Hola mundo")
+
+import random
+running = True
+randomnumber = random.randint(1, 10)
+user_attempts = 0
+computer_attepmts = 0
+while running: 
+    guess = int(input("Enter your number. "))
+    if guess == randomnumber:
+        user_attempts += 1
+        print("The player wins.")
+        print("It took the player", user_attempts, "attempt to get the right answer.")
+        running = False
+    elif guess <= randomnumber:
+        user_attempts += 1
+        print(guess, "is low.")
+    else:
+         user_attempts += 1
+         print(guess, "is high.")
+    pc_guess = random.randint(1, 10)
+    print(pc_guess)
+    if pc_guess == randomnumber:
+        computer_attepmts += 1
+        print("The computer wins.")
+        print("It took the computer", computer_attepmts, "attempt to get the right answer.")
+        running = False
+    elif pc_guess <= randomnumber:
+        computer_attepmts += 1
+        print(pc_guess, "is low.")
+    else: 
+        computer_attepmts += 1
+        print(pc_guess, "is high.")
