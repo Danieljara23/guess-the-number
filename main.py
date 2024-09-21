ee375c8b3d6d0671574dc45e458820b36e63afa9
Kalel Molano <contagiousmisfortune@gmail.com>
2024-09-20 21:36:53 -0500

    Añadida la función de jugar de nuevo

--- main.py
+++ main.py
@@ -1,33 +1,57 @@
 print("Hola mundo")
 
 import random
+
 running = True
+
 randomnumber = random.randint(1, 10)
+
 user_attempts = 0
+
 computer_attepmts = 0
-while running: 
-    guess = int(input("Enter your number. "))
-    if guess == randomnumber:
-        user_attempts += 1
-        print("The player wins.")
-        print("It took the player", user_attempts, "attempt(s) to get the right answer.")
-        running = False
-    elif guess <= randomnumber:
-        user_attempts += 1
-        print(guess, "is low.")
-    else:
-         user_attempts += 1
-         print(guess, "is high.")
-    pc_guess = random.randint(1, 10)
-    print(pc_guess)
-    if pc_guess == randomnumber:
-        computer_attepmts += 1
-        print("The computer wins.")
-        print("It took the computer", computer_attepmts, "attempt(s) to get the right answer.")
-        running = False
-    elif pc_guess <= randomnumber:
-        computer_attepmts += 1
-        print(pc_guess, "is low.")
-    else: 
-        computer_attepmts += 1
-        print(pc_guess, "is high.")
+
+while True:
+
+    while running: 
+        guess = int(input("Enter your number. "))
+        if guess == randomnumber:
+            user_attempts += 1
+            print("The player wins.")
+            print(f"It took the player {user_attempts} attempt(s) to get the right answer.")
+            running = False
+        elif guess <= randomnumber:
+            user_attempts += 1
+            print(f"{guess} is  low.")
+        else:
+            user_attempts += 1
+            print(f"{guess} is  high.")
+
+        if not running:
+                break
+
+        pc_guess = random.randint(1, 10)
+        print(pc_guess)
+        if pc_guess == randomnumber:
+            computer_attepmts += 1
+            print("The computer wins.")
+            print(f"It took the computer {computer_attepmts} attempts to get the right answer.")
+            running = False
+        elif pc_guess <= randomnumber:
+            computer_attepmts += 1
+            print(f"{pc_guess} is low.")
+        else: 
+            computer_attepmts += 1
+            print(f"{pc_guess} is high.")
+
+    while running == False:
+        play_again = input("Do you wish to play again? (y/n): ").lower()
+        
+        if play_again == 'y':
+            running = True
+            break
+        elif play_again == 'n':
+            print("Thank you for playing, see you around!")
+            exit()  
+        
+        else:
+            print("Alright, funny guy, I gave you 2 options. Stick to them. Y or N. Lowercase.")
